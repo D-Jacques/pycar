2c93001ae77107b83db75f21cdcba0c13076fad2
DUCROUX <jacques.ducroux@gmail.com>
2020-10-19 16:55:01 +0200

    Ajout des premiers fichiers, mise en place du fichier hello.py, simulation de connection

---
+++ pycar.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, make_response, abort, redirect, url_for
+from markupsafe import escape
+app = Flask(__name__)
+
+#It's the route for our main page, the user have to connect
+#with his logs to get farther
+@app.route('/connection')
+def connection():
+    return render_template('hello.html')
+
+#We access this page only if we send a form with POST method,
+#then we check if the written username is correct
+@app.route('/index', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        if request.form['username']:
+            resp = make_response(render_template('index.html', username = request.form['username']))
+            resp.set_cookie('username', request.form['username'])
+            return resp
+    else:
+        return redirect(url_for('connection'))
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('')
